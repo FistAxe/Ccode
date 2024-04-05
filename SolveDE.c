0aec8d13cfeecd3d5d4f6e78c01b67355fc63456
FistAxe <123456kth@naver.com>
2024-04-05 15:06:56 +0900

    Added manual REALtype, Narray size selection

--- SolveDE.c
+++ SolveDE.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
-#define REAL float
+//#define REAL float
 //#define REAL double
-#define Narray 100
+//#define Narray 100
 
-int SolveDE()
+int SolveDE(char REALtype, int arraysize)
 {
+    if(REALtype == 'f'){
+        #define REAL float
+    }
+
+    else if(REALtype == 'd'){
+        #define REAL double
+    }
+    
+    int Narray = arraysize;
+
     REAL zeta[Narray];
     REAL tauM = 3.0;
     REAL dtau = tauM/(Narray - 1);
@@ -19,15 +29,24 @@ int SolveDE()
     
     printf("%16s %16s %16s \n","index","tau","zeta");
     for(int i=0;i<Narray;i++){
-        printf("%d %f %f\n", i, dtau*i, (float)zeta[i]);
+        printf("%16d %f %f\n", i, dtau*i, (float)zeta[i]);
     }
     return 0;
 }
 
 int main(void){
-    printf("getting REAL type, Narray size\n");
-    scanf();
     
-    SolveDE();
+    int arraysize = 0;
+    char REALtype = '0';
+
+    printf("put REAL type.\n");
+    while(REALtype != 'f' && REALtype != 'd'){
+        printf("press \'f\' for float, \'d\' for double.\n");
+        scanf("%c", &REALtype);
+    }
+        printf("put array size.\n");
+        scanf("%d", &arraysize);
+    
+    SolveDE(REALtype, arraysize);
     return 0;
 }
